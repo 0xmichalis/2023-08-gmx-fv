e20e44fe4a0b2628632e8769068f4d2e2d4ed396
Michalis Kargakis <kargakis@protonmail.com>
2023-08-12 19:40:45 +0200

    Add rules for RoleStore

--- certora/harnesses/RoleStoreHarness.sol
+++ certora/harnesses/RoleStoreHarness.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 import "../../contracts/role/RoleStore.sol";
 
 contract RoleStoreHarness is RoleStore {
-    
+    function hasAdminRole() public view returns (bool) {
+        return hasRole(msg.sender, Role.ROLE_ADMIN);
+    }
 }
