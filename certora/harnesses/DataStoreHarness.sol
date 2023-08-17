f6c341087954c32eeedd6f8acde88eef40e10470
Michalis Kargakis <kargakis@protonmail.com>
2023-08-17 14:04:58 +0200

    Add rule to cover bug1.patch

--- certora/harnesses/DataStoreHarness.sol
+++ certora/harnesses/DataStoreHarness.sol
@@ -2,7 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "../../contracts/data/DataStore.sol";
+import "../../contracts/role/Role.sol";
 
 contract DataStoreHarness is DataStore {
     constructor(RoleStore _roleStore) DataStore(_roleStore) {}
+
+    function hasControllerRole() public view returns (bool) {
+        return roleStore.hasRole(msg.sender, Role.CONTROLLER);
+    }
 }
